b24c62af41e2122e531f2f1313043f120a6f43d1
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-09-01 06:30:39 +0200

    Add doc

--- HashType.pm
+++ HashType.pm
@@ -42,3 +42,129 @@ sub BUILD {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Data::HashType - Data object for hash type.
+
+=head1 SYNOPSIS
+
+ use Data::HashType;
+
+ my $obj = Data::HashType->new(%params);
+ my $active = $obj->active;
+ my $id = $obj->id;
+ my $name = $obj->name;
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = Data::HashType->new(%params);
+
+Constructor.
+
+=over 8
+
+=item * C<active>
+
+Flag for activity of hash type.
+Possible valuea are 0/1.
+Default value is 1 (active).
+
+=item * C<id>
+
+Id of record.
+Id could be number.
+It's optional.
+Default value is undef.
+
+=item * C<name>
+
+Hash type name.
+Maximal length of value is 50 characters.
+It's required.
+
+=back
+
+Returns instance of object.
+
+=head2 C<active>
+
+ my $active = $obj->active;
+
+Get active flag.
+
+Returns 0/1.
+
+=head2 C<id>
+
+ my $id = $obj->id;
+
+Get hash type record id.
+
+Returns number.
+
+=head2 C<name>
+
+ my $name = $obj->name;
+
+Get hash type name.
+
+Returns string.
+
+=head1 EXAMPLE
+
+=for comment filename=create_and_print_hash_type.pl
+
+ use strict;
+ use warnings;
+
+ use Data::HashType;
+
+ my $obj = Data::HashType->new(
+         'active' => 1,
+         'id' => 10,
+         'name' => 'SHA256',
+ );
+
+ # Print out.
+ print 'Name: '.$obj->name."\n";
+ print 'Active: '.$obj->active."\n";
+ print 'Id: '.$obj->id."\n";
+
+ # Output:
+ # Name: SHA256
+ # Active: 1
+ # Id: 10
+
+=head1 DEPENDENCIES
+
+L<Mo>,
+L<Mo::utils>.
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Data-HashType>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2023 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
