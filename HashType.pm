5eb3dbd48a1d082c2d2e9388a5d14d8c40035de8
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-03-01 00:35:27 +0100

    Prepare for 0.04 version

--- HashType.pm
+++ HashType.pm
@@ -6,7 +6,7 @@ use warnings;
 use Mo qw(build is);
 use Mo::utils 0.09 qw(check_bool check_length check_number check_required);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 has active => (
 	is => 'ro',
@@ -177,6 +177,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
