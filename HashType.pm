9039b0f16663d1b0378c9fa1d6bf4b40c758c05f
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-05-27 10:13:04 +0200

    Prepare for 0.07 version

--- HashType.pm
+++ HashType.pm
@@ -8,7 +8,7 @@ use Error::Pure qw(err);
 use Mo qw(build is);
 use Mo::utils 0.28 qw(check_isa check_length check_number_id check_required);
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 has id => (
 	is => 'ro',
@@ -232,6 +232,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
